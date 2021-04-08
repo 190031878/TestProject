dcba67cc75e3cfbaead171e68629dccf9c635686
Surabhi Yadav <Surabhi Yadav@LAPTOP-T0J2D1K9>
2021-04-08 15:03:27 +0530

    version 1.0

---
+++ JUnitFrameworkDemo/src/demo/FindResult.java
@@ -0,0 +1,34 @@
+package demo;
+
+public class FindResult
+{
+	public static int FindMax(int[] arr)
+	{
+		int max =arr[0];
+	     
+	    for (int i = 1; i < arr.length; i++)   
+	        {  
+	             
+	                if (max < arr[i])   
+	                {  
+	                    max=arr[i];
+	                }  
+	            }  
+	        
+	    return max;
+	}
+	public static boolean isEvenNum(int n )
+	  {
+	    boolean isFlag=false;
+	    if(n%2==0)
+	    {
+	      isFlag=true;
+	      
+	    }
+	    else
+	    {
+	      isFlag=false;
+	    }
+	    return isFlag;
+	  }
+}
